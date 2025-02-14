4111fcd48836bf2a1f4c53ccf5e354ef3f5b9313
luizguil99 <124329344+luizguil99@users.noreply.github.com>
2025-02-14 17:18:56 -0300

    Update chat.$id.tsx

--- app/routes/chat.$id.tsx
+++ app/routes/chat.$id.tsx
@@ -1,8 +1,20 @@
 import { json, type LoaderFunctionArgs } from '@remix-run/cloudflare';
 import { default as IndexRoute } from './_index';
+import { requireAuth } from '~/lib/auth';
+import { useLoaderData } from '@remix-run/react';
+import { ClientOnly } from 'remix-utils/client-only';
 
 export async function loader(args: LoaderFunctionArgs) {
+  await requireAuth(args);
   return json({ id: args.params.id });
 }
 
-export default IndexRoute;
+export default function ChatRoute() {
+  const { id } = useLoaderData<typeof loader>();
+  
+  return (
+    <ClientOnly fallback={null}>
+      {() => <IndexRoute />}
+    </ClientOnly>
+  );
+}
